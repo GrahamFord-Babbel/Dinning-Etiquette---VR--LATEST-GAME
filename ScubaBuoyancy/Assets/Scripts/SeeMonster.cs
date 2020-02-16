cff44a7c261db88cf02dd5fab0edf2b82f80ac72
Graham <graham.gettel@gmail.com>
2020-02-15 20:03:25 -0500

    Initial Commit - VR Setup + Physics

---
+++ ScubaBuoyancy/Assets/Scripts/SeeMonster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeMonster : MonoBehaviour
+{
+    public GameObject telescope;
+    public GameObject monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == telescope)
+        {
+            monster.gameObject.SetActive(true);
+            print("collision with telescope to head");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == telescope)
+        {
+            monster.gameObject.SetActive(false);
+        }
+    }
+}
