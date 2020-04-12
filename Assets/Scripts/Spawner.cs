f482f72ecf8a0aa4645c12f3d247e42b6b8eb118
Graham <graham.gettel@gmail.com>
2020-04-12 19:02:00 -0400

    Unity Purchasing package removed + Object Pooling conflicts with Normcore fixed

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -36,10 +36,14 @@ public class Spawner : MonoBehaviour {
 
     //bool of if all bottles active to stop coroutine
     public bool allBottlesActive;
+    public int countActive;
+    public bool oneSetActive;
 
     //hold random value - to prevent instantiating in same position
     public int previousRange;
     public int nextRandomRange;
+
+    private IEnumerator coroutine;
 
     private void Awake()
     {
@@ -47,6 +51,7 @@ public class Spawner : MonoBehaviour {
         _realtime = GetComponent<Realtime>();
 
         _realtime.didConnectToRoom += DidConnectToRoom;
+
     }
 
     // Use this for initialization
@@ -57,8 +62,9 @@ public class Spawner : MonoBehaviour {
         // Notify us when Realtime successfully connects to the room
         _realtime.didConnectToRoom += DidConnectToRoom;
 
-        //TODO: is there a way to put the PoolSystem here
-
+        //TODO: is there a way to put the PoolSystem here - not really because this script doesnt activate till player presses P?
+        //coroutine = TestRoutine(spawnWait);
+        //StartCoroutine(coroutine);
     }
 
     public void Update()
@@ -77,18 +83,18 @@ public class Spawner : MonoBehaviour {
             //activate bottles every spawnWait time
             //InvokeRepeating("ActivateBottles", Random.Range(0, 4), spawnWait);
 
+            //coroutine = ActivateObjectsTimer(spawnWait);
+            StartCoroutine(ActivateObjectsTimer(spawnWait));
+
             connectedSpawn = false;
         }
 
-        if(allBottlesActive == true)
+
+        if (allBottlesActive == true)
         {
             //StopCoroutine
+            print("COROUTINE STOPPED");
             StopCoroutine(ActivateObjectsTimer(spawnWait));
-        }
-        else //when object is picked up by Dad, allBottlesActive set to false
-        {
-            //StartCoroutine
-            StartCoroutine(ActivateObjectsTimer(spawnWait));
         }
 
         ////NORMCORE - Multiplayer Script - if connected Spawn Objects
@@ -106,6 +112,12 @@ public class Spawner : MonoBehaviour {
             fork.SetActive(true);
             forkDropped = true;
             print("the FORK has been dropped! Activate Audio HERE!");
+        }
+
+        else //when object is picked up by Dad, allBottlesActive set to false
+        {
+            //StartCoroutine
+            //StartCoroutine(ActivateObjectsTimer(spawnWait));
         }
     }
 
@@ -127,7 +139,7 @@ public class Spawner : MonoBehaviour {
                                 position: transform.position,          // Start 1 meter in the air
                                 rotation: transform.rotation * Quaternion.Euler(-90f, 0f, 0f), // No rotation
                            ownedByClient: false,   // Make sure the RealtimeView on this prefab is owned by this client
-               preventOwnershipTakeover: true,                // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
+               preventOwnershipTakeover: false,                // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
                             useInstance: _realtime);
 
         return babyBottle;
@@ -148,29 +160,60 @@ public class Spawner : MonoBehaviour {
     {
             RandomizeSpawn();
 
-            ////set position
-            //throwList[0].gameObject.transform.position = spawnLocations[nextRandomRange].position;
+        ////set position
+        //throwList[0].gameObject.transform.position = spawnLocations[nextRandomRange].position;
 
-            ////add object back to Baby Table
-            //throwList[0].SetActive(true);
+        ////add object back to Baby Table
+        //throwList[0].SetActive(true);
 
-            //TODO - use this for something fun
-            foreach (GameObject bottle in throwList)
+        //TODO - use this for something fun
+        foreach (GameObject bottle in throwList)
+        {
+
+            //    for (int i = 0; i < throwList.Count; i++)
+            //    { 
+            //        if (throwList[i].activeSelf == true)
+            //        {
+            //            countActive++;
+            //        }
+            //}
+            if (bottle.activeSelf == false && oneSetActive == false)
             {
-                if(bottle.activeSelf == false)
-                {
-                    bottle.transform.position = spawnLocations[nextRandomRange].position;
-                    bottle.transform.rotation = transform.rotation * Quaternion.Euler(-90f, 0f, 0f);
-                    bottle.SetActive(true);
+                //bottle.GetComponent<CubePl>
+                //._realtimeTransform.RequestOwnership();
 
-                //can i use return in a Void Function? Or break?
-                return;
-                }
-                else
+                //disable NORMCORE RT so you can relocate the object, will renable
+                //bottle.GetComponent<RealtimeTransform>().enabled = false;
+                bottle.GetComponent<RealtimeTransform>().RequestOwnership();
+                //relocate object - TODO: Need to add Realtime Transform to this somehow
+                bottle.transform.position = spawnLocations[nextRandomRange].position;
+                bottle.transform.rotation = transform.rotation * Quaternion.Euler(-90f, 0f, 0f);
+                bottle.SetActive(true);
+                //stop the pattern to repeat activating bottles
+                oneSetActive = true;
+            }
+
+            //if (bottle.activeSelf == false // &&count)
+            //{
+            //    bottle.transform.position = spawnLocations[nextRandomRange].position;
+            //    bottle.transform.rotation = transform.rotation * Quaternion.Euler(-90f, 0f, 0f);
+            //    bottle.SetActive(true);
+            //}
+            //if the last bottle is active, assume all bottles active (bad logic- TODO: Think better logic)
+            if(throwList[throwList.Count-1].activeSelf)
                 {
+                print("THROW LIST COOUNT:" + (throwList.Count - 1));
                 allBottlesActive = true;
+                countActive = 0;
                 }
-           }
+            else
+            {
+                print("doing nothing");
+            }
+            //THIS IS NOT DOING ANYTHING - being ignored
+            //return;
+        }
+        //return;
     }
 
     void RandomizeSpawn()
@@ -191,7 +234,22 @@ public class Spawner : MonoBehaviour {
     // every x seconds perform the print()
     private IEnumerator ActivateObjectsTimer(float waitTime)
     {
-        ActivateBottles();
-        yield return new WaitForSeconds(waitTime);
+        while (true)
+        {
+            ActivateBottles();
+            yield return new WaitForSeconds(waitTime);
+            //allow the pattern to repeat activating bottles
+            oneSetActive = false;
+            print("coroutine STILL running");
         }
+    }
+
+    private IEnumerator TestRoutine (float waitTime)
+    {
+        //needs while TRUE to function
+        print("coroutine 2 STILL running");
+        yield return new WaitForSeconds(waitTime);
+        print("coroutine 3 STILL running");
+    }
+
     }
