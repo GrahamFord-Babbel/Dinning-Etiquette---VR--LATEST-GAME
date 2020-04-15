8fe51b5cc1841419897d4ca1e0efddabef139bdd
Graham <graham.gettel@gmail.com>
2020-04-14 22:11:26 -0400

    Graham (#2)
    * realtimeProperty added for ScoreDisplay
    
    * further attempt to RealtimeProperty ScoreDisplay
    
    * hell of a collection of scribbled words + Normcore RealtimeModel attempt Score
    
    * Property Sync Functional (Gameplay Tested - Working)

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using Normal.Realtime;
 
-public class ScoreDisplay : MonoBehaviour
+public partial class ScoreDisplay: MonoBehaviour
 {
 
     //Script Purpose: To collect the current score from ScoreKeeper and display it for the user
@@ -11,17 +12,32 @@ public class ScoreDisplay : MonoBehaviour
     public Text scoreText;
     public Text timeText;
 
+    public float gameScore;
+
     // Use this for initialization
     void Start () {
         //scoreKeeper = FindObjectOfType<ScoreKeeper>();
         //scoreText = GetComponent<Text>();
+
+        //_scoreKeeper = GetComponent<ScoreKeeper>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //adjust gameScore from ScoreKeeper to here (because NORMCORE private restriction)
+        //TODO- somehow collect the Score Model's Score data when it changes
+        //gameScore = _scoreModel;
+
+        //added condition of this 4.14 after sync was working, if broken LOOK HERE
+        //only change score if actually changed
+        if(gameScore != scoreKeeper.gameScore)
+        {
+            gameScore = scoreKeeper.gameScore;
+        }
+
         //update the display to count updated score
-        scoreText.text = "Baby's Score: " + scoreKeeper.gameScore;
+        scoreText.text = "Baby's Score: " + gameScore;
         timeText.text = "Time Remaining: " + scoreKeeper.timeRemaining;
     }
 }
